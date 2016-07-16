37d6d252ed3cabf544868081fc1f817e0c613c5c
Mathias Westerdahl <mwesterdahl76@gmail.com>
2016-07-16 13:10:13 +0200

    First embryo of steering

---
+++ main/constants.lua
@@ -0,0 +1,17 @@
+
+local M = {}
+
+M.PLAYER_MESSAGE_ACCELERATE = hash("accelerate")
+M.PLAYER_MESSAGE_BRAKE 		= hash("brake")
+M.PLAYER_MESSAGE_LEFT 		= hash("left")
+M.PLAYER_MESSAGE_RIGHT 		= hash("right")
+M.PLAYER_MESSAGE_BOOST 		= hash("boost")
+M.PLAYER_MESSAGE_SPIN 		= hash("spin")
+
+
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+return M
