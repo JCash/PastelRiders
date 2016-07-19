5a136b1a02efb6c93a597818665e94232a5ea559
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-07-19 17:52:58 +0200

    First embryo of stencil fade in

---
+++ main/stencil_tile.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/images/stencils/stencil.atlas\"\n"
+  "default_animation: \"white\"\n"
+  "material: \"/main/material/stencil.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
